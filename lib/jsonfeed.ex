39d4a11b4f9d40bb39851a90f4d4ec5f2d00292e
José Carlos Joaquim <j.joaquim@gmail.com>
2017-05-25 01:00:51 +0200

    Initial commit

---
+++ lib/jsonfeed.ex
@@ -0,0 +1,18 @@
+defmodule Jsonfeed do
+  @moduledoc """
+  Documentation for Jsonfeed.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jsonfeed.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
